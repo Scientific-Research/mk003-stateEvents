1067330ebd2d414b3f7475c8038cfde0e41c2cb5
Maximilian <scientificresearch309@gmail.com>
2023-02-11 13:48:31 +0100

    Score and Count are no longer 0!

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(4);
-  const [score, setScore] = useState(7);
+  const [count, setCount] = useState(0);
+  const [score, setScore] = useState(0);
 
   const handleScore = () => {
     setScore(score + 1);
+    // setScore(score++);
   };
 
   return (
     <div className="App">
+      {score > 0 && count > 0 && <p>Score and Count are no longer 0!</p>}
       <button onClick={() => setCount((count) => count + 1)}>
         count is {count}
       </button>
