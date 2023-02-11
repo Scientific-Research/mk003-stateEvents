7bddf2742f3dbe09cdb908cf74b1810d85a7c961
Maximilian <scientificresearch309@gmail.com>
2023-02-11 21:26:12 +0100

    if (score > 5) {       setCount(0);     }

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
   const [score, setScore] = useState(0);
 
+  const handleCount = () => {
+    setCount(count + 1);
+    // console.log(count);
+  };
   const handleScore = () => {
     setScore(score + 1);
     // setScore(score++);
+    if (score > 5) {
+      setCount(0);
+    }
+    console.log(score);
   };
 
   return (
     <div className="App">
-      {score > 0 && count > 0 && <p>Score and Count are no longer 0!</p>}
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
+      {/* {score > 0 && count > 0 && <p>Score and Count are no longer 0!</p>} */}
+      {/* {score > 5 && setCount(count - count)} */}
+      {/* {score > 5 && setCount(0)} */}
+      <button onClick={handleCount}>count is {count}</button>
       <hr />
       <button onClick={handleScore}>score = {score}</button>
     </div>
